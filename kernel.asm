e8eeea532df6b92d3d5ce629f4fb24f6508eb3ab
teamUNI <boywondertcg@gmail.com>
2014-10-24 20:57:45 -0400

    Create kernel.asm

---
+++ kernel.asm
@@ -0,0 +1,5 @@
+;The UNIOS kernel;
+;you may redistribute and modify this kernel as long as it is under the GPL v2 license.;
+;if not,we will send you to jail or sue you.;
+;by:Team UNI;
+================================================================================================================================
